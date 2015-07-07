237e4b3b32fb3a456e59b96badd29c48c9bf6891
Tarrence Egbert <tmegbert@gmail.com>
2015-07-07 16:58:16 -0600

    cleaned house

--- createThumb.php
+++ createThumb.php
@@ -4,18 +4,23 @@ $imagePath = $argv[1];
 
     //Read original image and create Imagick object
     $image=new Imagick($imagePath);
+    $thumbImage = clone $image;
+    $midImage = clone $image;
+
     $d = $image->getImageGeometry(); 
     $w = $d['width']; 
     $h = $d['height'];
 
-    $newH = 500;
-    $newW = intval((500 / $h) * $w);
-    //echo "width=" . $newW . "\n";
-    //echo "height=" . $newH . "\n";
+    $midH = 500;
+    $midW = intval((500 / $h) * $w);
+    $thumbH = 200;
+    $thumbW = intval((200 / $h) * $w);
 
     //Scale the image
-    $image->thumbnailImage($newW, $newH);
+    $midImage->thumbnailImage($midW, $midH);
+    $thumbImage->thumbnailImage($thumbW, $thumbH);
 
     //Write the new image to a file
-    $image->writeImage('test_image.jpg');
+    $midImage->writeImage('mid_image.jpg');
+    $thumbImage->writeImage('thumb_image.jpg');
 ?>
